62681e8777f458e1e2143ba9b5d515ee30c2d13f
vitormdias <vitor.dias@ventron.com.br>
2017-07-10 17:53:31 -0300

    new structure added

---
+++ src/components/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <h2>Test It</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'home'
+}
+</script>
+
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+</style>
