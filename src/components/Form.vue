f1a91344d67549b5d668ee19454c7aafd425ff6f
medson10 <medson.oliveira@fatec.sp.gov.br>
2017-07-13 18:27:37 -0300

    Adiciona Validação

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -13,13 +13,15 @@
 
       <md-input-container>
         <label>Senha</label>
-        <md-input type="password" required></md-input>
+        <md-input type="password" required v-model="pwd"></md-input>
       </md-input-container>
 
       <md-input-container>
         <label>Confirme a Senha</label>
-        <md-input type="password" required></md-input>
+        <md-input type="password" required v-model="rpwd"></md-input>
       </md-input-container>
+
+      <span v-if="compare(pwd, rpwd) == false">*Senhas não coincidem</span>
 
       <md-input-container>
         <label>Data de Nascimento</label>
@@ -48,9 +50,46 @@
 
 <script>
 export default {
-  name: 'vform'
+  name: 'vform',
+  data () {
+    return {
+      pwd: '',
+      rpwd: ''
+    }
+  },
+  methods: {
+    compare: function (pwd, rpwd) {
+      if (pwd === rpwd) {
+        return true
+      } else {
+        // console.log('senha errada')
+        return false
+      }
+    }
+  }
 }
 </script>
 
 <style lang="css">
+.vform span {
+  color: #F00;
+}
+
+@media only screen
+and (min-device-width : 320px)
+and (max-device-width : 568px)
+and (orientation: portrait) {
+
+  .vform {
+    width: 100%;
+    min-width: 40vw;
+  }
+
+  .vform md-input-container {
+    width: 100%;
+    min-width: 40vw;
+  }
+
+}
+
 </style>
