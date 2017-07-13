c5e4495dcb8f4445e658fd583bfb6891134f19e1
medson10 <medson.oliveira@fatec.sp.gov.br>
2017-07-13 01:55:14 -0300

    Add login component

---
+++ src/components/Login.vue
@@ -0,0 +1,28 @@
+<template lang="html">
+  <div class="vlogin">
+    <form novalidate @submit.stop.prevent="submit">
+
+      <md-input-container>
+        <label>Email</label>
+        <md-input type="email"></md-input>
+      </md-input-container>
+
+      <md-input-container>
+        <label>Senha</label>
+        <md-input type="password"></md-input>
+      </md-input-container>
+
+      <md-button class="md-raised md-primary">Login</md-button>
+
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'vlogin'
+}
+</script>
+
+<style lang="css">
+</style>
