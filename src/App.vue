8c98db67b8a2848301ba56fa008e54dc950ea3d7
medson10 <medson.oliveira@fatec.sp.gov.br>
2017-07-12 23:19:32 -0300

    Add Vue Material

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app-main">
+  <div>
     <VHeader></VHeader>
     <div id="app-container">
       <router-view></router-view>
@@ -21,5 +21,4 @@ export default {
 }
 </script>
 
-<style>
-</style>
+<style src="./assets/styles/main.css"></style>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,22 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import VueMaterial from 'vue-material'
 import App from './App'
 import router from './router'
 
+Vue.use(VueMaterial)
 Vue.config.productionTip = false
+
+Vue.material.registerTheme('default', {
+  primary: {
+    color: 'indigo',
+    hue: 900
+  },
+  accent: 'blue',
+  warn: 'black',
+  background: 'grey'
+})
 
 /* eslint-disable no-new */
 new Vue({
