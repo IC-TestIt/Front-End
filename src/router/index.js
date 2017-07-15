5a147a9024fdb1c2d64d1d1b64516e4e93242e44
medson10 <medson.oliveira@fatec.sp.gov.br>
2017-07-14 23:18:42 -0300

    Renomeia componente FinishSignUp

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/components/Home'
-import ConcludeSignUp from '@/components/ConcludeSignUp'
+import FinishSignUp from '@/components/FinishSignUp'
 
 Vue.use(Router)
 
@@ -13,7 +13,7 @@ export default new Router({
   },
   {
     path: '/signup',
-    name: 'ConcludeSignUp',
-    component: ConcludeSignUp
+    name: 'FinishSignUp',
+    component: FinishSignUp
   }]
 })
