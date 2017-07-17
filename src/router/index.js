9822aff974c4a7ee2493bd39d1a58a6a9a5a0e8f
medson10 <medson.oliveira@fatec.sp.gov.br>
2017-07-17 01:48:40 -0300

    Mapeia rota concluir cadastro

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/components/Home'
 import FinishSignUp from '@/components/FinishSignUp'
+import Main from '@/components/Main'
 
 Vue.use(Router)
 
@@ -12,8 +13,18 @@ export default new Router({
     component: Home
   },
   {
-    path: '/signup',
+    path: '/signup/',
     name: 'FinishSignUp',
     component: FinishSignUp
+  },
+  {
+    path: '/signup/:id',
+    name: 'FinishSignUp',
+    component: FinishSignUp
+  },
+  {
+    path: '/home',
+    name: 'Main',
+    component: Main
   }]
 })
