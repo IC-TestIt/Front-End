60a71045d1725641c32858d7fa9189355f6e2c32
vitormdias <vitormd97@gmail.com>
2017-08-09 21:36:58 -0300

    adiciona service de autenticação

---
+++ src/auth.js
@@ -0,0 +1,33 @@
+import baseService from './services/baseService'
+
+export default {
+  logIn (email, password) {
+    return baseService.login(email, password).then(response => {
+      if (response.status === 200) {
+        localStorage.setItem('token', response.data.access_token)
+        localStorage.setItem('userId', response.data.userId)
+        localStorage.setItem('teacherId', response.data.teacherId)
+        localStorage.setItem('studentId', response.data.studentId)
+        Promise.resolve(response.status)
+      }
+    })
+  },
+  logout () {
+    localStorage.clear()
+  },
+  loggedIn () {
+    return localStorage.getItem('token') !== null
+  },
+  isTeacher () {
+    return localStorage.getItem('teacherId') !== null
+  },
+  isStudent () {
+    return localStorage.getItem('studentId') !== null
+  },
+  teacherId () {
+    return localStorage.getItem('teacherId')
+  },
+  studentId () {
+    return localStorage.getItem('studentId')
+  }
+}
