fca31d87297f45a983e838635059641d5694ff27
vitormdias <vitor.dias@ventron.com.br>
2017-08-14 13:31:43 -0300

    Ajusta layout

--- src/components/MyTests.vue
+++ src/components/MyTests.vue
@@ -2,10 +2,10 @@
     <div class="my-tests">
         <v-layout row wrap>
             <v-flex xs12>
-                <h3 class="my-tests-title text-xs-center ma-1 pa-3">Minhas Provas</h3>
+                <h3 class="my-tests-title text-xs-center ma-1 pt-4">Minhas Provas</h3>
             </v-flex>
             <v-flex xs12 md4>
-                <v-card class="orange darken-1 white--text ma-5">
+                <v-card class="green darken-1 white--text ma-5 text-xs-center">
                     <v-card-title primary-title>
                         <v-flex xs12>
                             <div class="headline">0</div>
@@ -17,7 +17,7 @@
                 </v-card>
             </v-flex>
             <v-flex xs12 md4>
-                <v-card class="indigo lighten-1 white--text ma-5">
+                <v-card class="indigo lighten-1 white--text ma-5 text-xs-center">
                     <v-card-title primary-title>
                         <v-flex xs12>
                             <div class="headline">0</div>
@@ -29,18 +29,18 @@
                 </v-card>
             </v-flex>
             <v-flex xs12 md4>
-                <v-card class="green darken-1 white--text ma-5">
+                <v-card class="orange darken-1 white--text ma-5 text-xs-center">
                     <v-card-title primary-title>
                         <v-flex xs12>
                             <div class="headline">0</div>
                         </v-flex>
                         <v-flex xs12>
-                            <div class="pt-3">Alguma Coisa</div>
+                            <div class="pt-3">Provas Pendentes</div>
                         </v-flex>
                     </v-card-title>
                 </v-card>
             </v-flex>
-            <v-flex xs0 md12 class="ma-5 pa-1">
+            <v-flex xs0 md12 class="mr-5 ml-5 pa-1">
                 <v-data-table 
                     v-bind:headers="headers" 
                     :items="tests" 
@@ -52,7 +52,7 @@
                         <td class="text-xs-center">{{ props.item.Description }}</td>
                         <td class="text-xs-center">
                             <v-dialog v-model="dialog" scrollable>
-                                <v-btn primary dark slot="activator" @click.native="dialog = true">Open Dialog</v-btn>
+                                <v-btn primary dark slot="activator" @click.native="dialog = true">Aplicar</v-btn>
                                 <v-card>
                                     <v-card-title>Selecione a Turma</v-card-title>
                                     <v-divider></v-divider>
