923c9297b576d2e228d52ea96b71581cad42d9a8
matheusnovais95 <mths_campos@hotmail.com>
2017-08-18 13:52:36 -0300

    Dashboard não aparece

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,74 +1,39 @@
-<template lang="html">
-  <!--<div class="main has-ripple blue">
-    <div v-show="loggedIn" class="logout">
-      <a v-on:click="logOut()">
-        <i class="fa fa-sign-out" aria-hidden="true"></i>
-      </a>
-    </div>
-    <md-ink-ripple />
-    <md-list class="list">
-      <md-toolbar md-theme="white">
-        <span class="md-title">Menu</span>
-      </md-toolbar>
-      <md-divider></md-divider>
-      <md-list-item href="#/home">Provas</md-list-item>
-      <md-list-item href="#/home">Turmas</md-list-item>
-      <md-list-item href="#/home">Configurações</md-list-item>
-    </md-list>-->
-    <!-- <div class="dash">
-
-      <md-whiteframe class="a dash-item has-ripple blue">
-        <md-ink-ripple />
-        <md-icon class="md-size-4x md-accent">subject</md-icon>
-        <span>Provas</span>
-      </md-whiteframe>
-
-      <md-whiteframe class="b dash-item has-ripple blue">
-        <md-ink-ripple />
-        <md-icon class="md-size-4x md-accent">border_color</md-icon>
-        <span>Realizar Prova</span>
-
-      </md-whiteframe>
-
-      <md-whiteframe class="c dash-item has-ripple blue">
-        <md-ink-ripple />
-        <md-icon class="md-size-4x md-accent">edit</md-icon>
-        <span>Provas Realizadas</span>
-
-
-      </md-whiteframe>
-
-      <md-whiteframe class="d dash-item has-ripple blue">
-        <md-ink-ripple />
-        <md-icon class="md-size-4x md-accent">check</md-icon>
-        <span>Notas</span>
-
-
-      </md-whiteframe>
-
-      <md-whiteframe class="e dash-item has-ripple blue">
-        <md-ink-ripple />
-        <md-icon class="md-size-4x md-accent">folder</md-icon>
-        <span>Materiais de Consulta</span>
-
-
-      </md-whiteframe>
-
-      <md-whiteframe class="f dash-item has-ripple blue">
-        <md-ink-ripple />
-        <md-icon class="md-size-4x md-accent">trending_up</md-icon>
-        <span>Estatisticas</span>
-
-      </md-whiteframe> -->
-
-    </div>
-  </div>
-
+<template> 
+      <div class="main">
+        <div >
+           <VMenu></VMenu>               
+        </div>
+        <v-container fluid>         
+            <v-layout row wrap>
+              <v-flex xs3 v-for="i in 4" :key="i" class="menus">
+                <v-card dark class="card-panel white elevation-1 mt-4">
+                  <div class="py-5">
+                    <v-card-text class="black--text title">1</v-card-text>
+                  </div>
+                </v-card>
+              </v-flex>
+              <v-flex xs12>
+                <v-card dark class="card-panel white elevation-1  mt-1 py-5">
+                  <div class="my-4">
+                    <v-card-text class="black--text display-1">Grafico</v-card-text>
+                  </div>
+                </v-card>
+              </v-flex>
+            </v-layout>       
+        </v-container>
+      
+      </div>
 </template>
 
 <script>
+
+import VMenu from './MenuBar'
+
 export default {
   name: 'main',
+  components: {
+    VMenu
+  },
   data () {
     return {
       loggedIn: false
@@ -86,105 +51,3 @@ export default {
   }
 }
 </script>
-
-<style lang="css">
-
-.main {
-  display: flex;
-  min-height: 80vh;
-  min-width: 40vw;
-  background-color: #ECECEC;
-  height: 200px;
-}
-
-.list {
-  display: flex;
-  /*align-items: center;*/
-  text-decoration: none;
-  border-right: 1px solid #000;
-  border-radius: 2px;
-}
-
-.dash {
-  display: grid;
-  width: 100%;
-  height: 100%;
-  margin: 20px;
-  grid-gap: 20px;
-  grid-template-columns: auto;
-  grid-template-rows: 220px 220px;
-  grid-template-areas:
-    'a b c '
-    'd e f '
-  ;
-}
-
-.a {
-  grid-area: a;
-}
-
-.b {
-  grid-area: b;
-}
-
-.c {
-  grid-area: c;
-}
-
-.d {
-  grid-area: d;
-}
-
-.e {
-  grid-area: e;
-}
-
-.f {
-  grid-area: f;
-}
-
-.dash-item {
-  display: flex;
-  height: 100%;
-  width: 100%;
-  /*justify-content: center;*/
-  align-items: center;
-  flex-direction: column;
-  background-color: #FAFAFA;
-}
-
-hr {
-  color: #000;
-  width: 100%;
-}
-
-.list span{
-  color:#FFF;
-}
-
-.list a {
-  color: #FFF;
-}
-
-.dash-item span {
-  /*text-decoration: none;*/
-  /*color: #FF8F00;*/
-  color: #006;
-  margin: 10px;
-  text-transform: uppercase;
-  letter-spacing: 1px;
-}
-
-.logout {
-  display: flex;
-  align-items: center;
-  position: absolute;
-  left: 90vw;
-  top: 2vh;
-  color: #006;
-}
-
-.logout i {
-  font-size: 5vh;
-}
-</style>
--- src/components/MenuBar.vue
+++ src/components/MenuBar.vue
@@ -1,25 +1,73 @@
 <template>
-
+  <div class="menuBar">        
+      <v-navigation-drawer persistent light overflow class="grey darken-1">
+        <v-toolbar flat class="transparent">
+          <v-list class="pa-0">
+            <v-list-tile avatar tag="div" class="light-blue darken-1 my-1 py-3">
+              <v-list-tile-avatar>
+                <img src="" />
+              </v-list-tile-avatar>
+              <v-list-tile-content>
+                <v-list-tile-title class="white--text">Bem-vindo</v-list-tile-title>
+              </v-list-tile-content>
+            </v-list-tile>
+          </v-list>
+        </v-toolbar>
+        <v-list class="grey darken-1 my-4">
+          <v-list-group v-for="item in items" :value="item.active" v-bind:key="item.title">
+            <v-list-tile slot="item">
+              <v-list-tile-action>
+                <v-icon class="world">{{ item.action }}</v-icon>
+              </v-list-tile-action>
+              <v-list-tile-content>
+                <v-list-tile-title class="world">{{ item.title }}</v-list-tile-title>
+              </v-list-tile-content>
+              <v-list-tile-action>
+                <v-icon>keyboard_arrow_down</v-icon>
+              </v-list-tile-action>
+            </v-list-tile>
+            <v-list-tile v-for="subItem in item.items" v-bind:key="subItem.title" class="grey lighten-1">
+              <v-list-tile-content>
+                <v-list-tile-title>{{ subItem.title }}</v-list-tile-title>
+              </v-list-tile-content>
+              <v-list-tile-action>
+                <v-icon>{{ subItem.action }}</v-icon>
+              </v-list-tile-action>
+            </v-list-tile>
+          </v-list-group>
+        </v-list>
+      </v-navigation-drawer>         
+  </div>
 </template>
+
 <script>
 export default {
-  name: "menuBar",
+  name: 'menuBar',
   data () {
-    loggedIn: false
-
-  },
-  mounted () {
-    this.loggedIn = localStorage.getItem('token') !== null
-  },
-  methods: {
-    logOut: function () {
-      localStorage.clear()
-      this.loggedIn = false
-      this.$router.push('/')
+    return {
+      items: [
+        {
+          action: 'create',
+          title: 'PROVAS',
+          items: [
+                { title: 'Cadastrar' },
+                { title: 'Minhas Provas' }
+          ]
+        },
+        {
+          action: 'people',
+          title: 'TURMAS',
+          active: false,
+          items: [
+                { title: 'Item' },
+                { title: 'Item' }
+          ],
+          right: true,
+          rightDrawer: true
+        }
+      ]
     }
   }
 }
 </script>
 
-<style>
-</style>
