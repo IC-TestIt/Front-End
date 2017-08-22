a31b8bff9c9346737fc50f756061a871f57cd73b
Medson <medson.oliveira.jr@gmail.com>
2017-08-22 03:14:24 -0300

    Remove metodo vazio

--- src/components/ReviewTest.vue
+++ src/components/ReviewTest.vue
@@ -3,13 +3,13 @@
     <v-card class="lighten-1 z-depth-1 mb-5 pa-5" height="200px">
       <v-layout align-center>
         <v-flex xs3 fill-height>
-          <v-btn class="CreateTest-Step3Button green lighten-1 pa-5">Associar Prova</v-btn>
+          <v-btn class="CreateTest-step-3-button green lighten-1 pa-5">Associar Prova</v-btn>
         </v-flex>
         <v-flex xs3 fill-height>
-          <v-btn class="CreateTest-Step3Button indigo lighten-1 pa-5">Visualizar Prova</v-btn>
+          <v-btn class="CreateTest-step-3-button indigo lighten-1 pa-5">Visualizar Prova</v-btn>
         </v-flex>
         <v-flex xs3 fill-height>
-          <v-btn class="CreateTest-Step3Button red lighten-1 pa-5">Exportar Prova</v-btn>
+          <v-btn class="CreateTest-step-3-button red lighten-1 pa-5">Exportar Prova</v-btn>
         </v-flex>
       </v-layout>
     </v-card>
@@ -19,9 +19,7 @@
 </template>
 <script>
 export default {
-  name: 'reviewTest',
-  data: () => ({
-  })
+  name: 'reviewTest'
 }
 </script>
 <style lang="css" scoped>
