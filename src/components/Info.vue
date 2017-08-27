f64d2ea028159ec2e800c5a40c3d1501886bf81d
Medson de Oliveira Junior <medson.oliveira.jr@gmail.com>
2017-08-27 00:29:15 -0300

    Feat/add minhas avaliacoes (#31)
    * Remove espaço a mais
    
    * Adiciona componente MyExams
    
    * Adiciona rota /avaliacoes

--- src/components/Info.vue
+++ src/components/Info.vue
@@ -1,6 +1,5 @@
-<template>      
-        
-        <v-container>         
+<template>
+        <v-container>
             <v-layout class="text-xs-center mt-3">
                <v-flex xs5>
                  <v-card dark class="mx-2 green lighten-1" >
@@ -16,17 +15,16 @@
                  <v-card dark class=" mx-2 orange darken-1">
                     <v-card-text class="py-5">Estatistica 3</v-card-text>
                  </v-card>
-               </v-flex>              
+               </v-flex>
              </v-layout>
              <v-layout class="">
                <v-flex xs12 class="mt-5 text-xs-center">
-                <v-card dark class="card-panel white elevation-1 py-5 px-5">                  
-                    <v-card-text class="black--text display-1 my-5">Grafico</v-card-text>                  
+                <v-card dark class="card-panel white elevation-1 py-5 px-5">
+                    <v-card-text class="black--text display-1 my-5">Grafico</v-card-text>
                 </v-card>
-              </v-flex>              
-            </v-layout>     
+              </v-flex>
+            </v-layout>
         </v-container>
-      
 </template>
 
 <script>
@@ -40,4 +38,3 @@ export default {
     text-align: center;
   }
 </style>
-
