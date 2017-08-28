574e4831b8af7d28a0d08d9a2b47ec96b88e6ec1
Medson <medson.oliveira.jr@gmail.com>
2017-08-28 15:45:31 -0300

    Adiciona endereço PDF

--- config/dev.env.js
+++ config/dev.env.js
@@ -3,5 +3,6 @@ var prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  API: '"http://localhost:59927"'
+  API: '"http://localhost:59927"',
+  PDF: '"http://localhost:3000"'
 })
