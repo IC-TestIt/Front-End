c9a5ab8a2d404588cc1aa22680d92bd42e210180
Medson de Oliveira Junior <medson.oliveira.jr@gmail.com>
2017-08-29 09:15:52 -0300

    Feat/add exportar pdf (#32)
    * Adiciona endereço PDF
    
    * Integra exportar PDF
    
    * Corrige bug
    
    * Adiciona pdfService
    
    * Correções Menores

--- src/components/ReviewTest.vue
+++ src/components/ReviewTest.vue
@@ -9,18 +9,31 @@
           <v-btn class="indigo darken-1 pa-2" dark="true">Visualizar Prova<v-icon right dark>chrome_reader_mode</v-icon></v-btn>
         </v-flex>
         <v-flex xs3>
-          <v-btn class="amber darken-1 pa-2" light>Exportar Prova<v-icon right dark>get_app</v-icon></v-btn>
+          <v-btn class="amber darken-1 pa-2" light v-on:click="exportPdf()">Exportar Prova<v-icon right dark>get_app</v-icon></v-btn>
         </v-flex>
       </v-layout>
     </v-card>
-    <v-btn primary @click.native="e1 = 1">Finalizar</v-btn>
-    <v-btn flat @click.native="e1 = 2">Cancel</v-btn>
   </div>
 </template>
+
 <script>
+import baseService from '../services/baseService'
+import pdfService from '../services/pdfService'
+
 export default {
-  name: 'reviewTest'
+  name: 'reviewTest',
+  props: ['testId'],
+  methods: {
+    exportPdf: function () {
+      baseService.get(`/test/export/${this.testId}`).then(r => {
+        let pdf = {'content': r.data}
+        pdfService.post(`/pdf`, pdf).then(r => {
+          if (r.status === 200) {
+            window.open(pdfService.get(), '_blank')
+          }
+        })
+      })
+    }
+  }
 }
 </script>
-<style lang="css" scoped>
-</style>
