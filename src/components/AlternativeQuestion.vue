75af6d0e48558aae0a1a19c4d582e39e3edebde3
Medson <medson.oliveira.jr@gmail.com>
2017-09-06 17:36:05 -0300

    Adiciona componente AlternativeQuestion

---
+++ src/components/AlternativeQuestion.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="alternativeQuestion">
+    <div v-for="alternative in question.alternatives" :key="alternative.key">
+      <v-radio class="alternativeQuestion-alternative" color="primary" @change="realizeQuestion()" :label="alternative.description" v-model="realizeQuestion.alternativeId" :value="alternative.id"></v-radio>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'alternativeQuestion',
+  props: ['question', 'realizeQuestion'],
+  methods: {
+    realizeQuestion () {
+      this.$emit('get-alternative', this.realizeQuestion)
+    }
+  }
+}
+</script>
+<style lang="css" scoped>
+
+.alternativeQuestion-alternative {
+  margin-bottom: -40px;
+}
+
+</style>
