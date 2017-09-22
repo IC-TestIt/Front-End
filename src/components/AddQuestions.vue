984872706f560a4f6c3407cbaaba97e2f628e49b
Medson <medson.oliveira.jr@gmail.com>
2017-09-22 01:39:40 -0300

    Refatora layout e adiciona algumas correções

--- src/components/AddQuestions.vue
+++ src/components/AddQuestions.vue
@@ -1,20 +1,25 @@
 <template>
   <div class="addQuestion">
-    <v-card class="white lighten-1 z-depth-1 mb-1">
+    <div class="white lighten-1 z-depth-1">
       <v-layout column wrap>
         <form class="createTest-QuestionForm" v-on:submit="step2($event)">
           <div class="">
-            <DynamicList @get-current='getCurrentQuestion' @get-index='getIndex' :list='questions' :current='currentQuestion'></DynamicList>
-            <v-flex xs12 class="ma-3 pa-3">
+            <v-flex xs12 class="ma-1 pa-2">
+              <v-layout row wrap>
+                <v-flex class="ma-1 pa-2" xs8>
+                  <DynamicList @get-current='getCurrentQuestion' @get-index='getIndex' :list='questions' :current='currentQuestion'></DynamicList>
+                </v-flex>
+                <v-flex class="ma-1 pa-2" xs3>
+                  <v-btn class="green" v-on:click="addQuestion()" fab small dark><v-icon>add</v-icon></v-btn>
+                  <v-btn class="red" v-on:click="removeQuestion()" fab small dark><v-icon>remove</v-icon></v-btn>
+                  <v-btn primary type="submit" :loading="loading" dark>salvar</v-btn>
+                </v-flex>
+              </v-layout>
+            </v-flex>
+            <v-flex xs12 class="ma-1 pa-2 text-xs-center">
               <h6>Questão {{index}}</h6>
             </v-flex>
-            <v-flex xs12 class="ma-1 pa-2">
-              <v-text-field label="Descrição da Questão" v-model="currentQuestion.description"></v-text-field>
-            </v-flex>
-            <v-flex xs12 class="ma-1 pa-2">
-              <v-text-field label="Valor da Questão" v-model="currentQuestion.value"></v-text-field>
-            </v-flex>
-            <v-flex xs12 class="ma-1 pa-2">
+            <v-flex xs8 class="ma-1 pa-2">
               <v-layout row wrap class="dark--text">
                 <v-flex xs12 sm6 class="py-2">
                   <p>Tipo de Questão</p>
@@ -22,50 +27,59 @@
                 </v-flex>
               </v-layout>
             </v-flex>
-            <v-flex xs12 v-if="!currentQuestion.isAlternative" class="ma-1 pa-2">
-              <v-text-field label="Resposta da Questão" v-model="currentQuestion.answer"></v-text-field>
-            </v-flex>
-            <v-flex xs12 v-if="!currentQuestion.isAlternative" class="ma-1 pa-2">
+            <v-flex xs12>
               <v-layout row wrap>
-                <v-flex xs6>
-                  <v-text-field label="Palavras chave" v-model="keyword"></v-text-field>
+                <v-flex xs5 class="mr-1 pa-2">
+                  <v-text-field label="Descrição da Questão" v-model="currentQuestion.description" multi-line></v-text-field>
                 </v-flex>
-                <v-flex xs6>
-                  <v-btn class="indigo" v-on:click="addKeyword()" v-if="!currentQuestion.isAlternative" dark>Adicionar Palavra-Chave</v-btn>
+                <v-flex xs5 class="ma-1 pa-2">
+                  <v-text-field label="Resposta da Questão" v-if="!currentQuestion.isAlternative" v-model="currentQuestion.answer" multi-line></v-text-field>
+                </v-flex>
+              </v-layout>
+            </v-flex>
+            <v-flex xs12 class="ma-1 pa-2">
+              <v-layout row wrap>
+                <v-flex xs4 class="ma-1">
+                  <v-text-field v-if="!currentQuestion.isAlternative" label="Palavras chave" v-model="keyword"></v-text-field>
+                </v-flex>
+                <v-flex xs1 class="ma-1">
+                  <v-btn class="indigo" v-on:click="addKeyword()" v-if="!currentQuestion.isAlternative" fab small dark><v-icon dark>add</v-icon></v-btn>
+                </v-flex>
+                <v-flex xs6 class="ma-1">
+                  <v-layout row wrap>
+                    <div v-for="k in currentQuestion.keywords">
+                      <v-flex xs1>
+                        <v-chip class="indigo white--text" close @input="removeChip(k)">{{k}}</v-chip>
+                      </v-flex>
+                    </div>
+                  </v-layout>
                 </v-flex>
               </v-layout>
               <v-layout row wrap>
-                <div v-for="k in currentQuestion.keywords">
-                  <v-flex xs12>
-                    <v-chip class="indigo white--text" close @input="removeChip(k)">{{k}}</v-chip>
-                  </v-flex>
-                </div>
+                <v-flex xs5 class="mr-1 pa-2">
+                  <v-text-field label="Valor da Questão" v-model="currentQuestion.value"></v-text-field>
+                </v-flex>
               </v-layout>
             </v-flex>
-            <v-flex xs4 v-if="!currentQuestion.isAlternative" class="ma-1 pa-2">
-            </v-flex>
-            <div class="" v-for="(a, index) in currentQuestion.alternatives" :key="a.key">
+            <div class="" v-for="(a, index) in currentQuestion.alternatives" :key="a.key" v-if="currentQuestion.isAlternative">
               <v-layout row wrap>
-                <v-flex class="ma-2 pa-2" xs5 v-if="currentQuestion.isAlternative">
+                <v-flex class="ma-1 ml-2 pa-1 pl-2" xs5>
                   <v-text-field label="Descrição da Alternativa" v-model="a.description"></v-text-field>
+                </v-flex>
+                <v-flex class="ma-1 ml-2 pa-1 pl-2" xs2>
                   <v-radio color="primary" :tabindex="currentQuestion.index" v-model="a.isCorrect" label="Alternativa Correta" :value="true"></v-radio>
                 </v-flex>
-                <v-flex class="ma-2 pa-2" xs6 v-if="currentQuestion.isAlternative">
-                  <small class="CreateTest-WarningText" v-if="!alternativeLimit(currentQuestion) && lastAlternative(currentQuestion, index)">O numero maximo de alternativas é 5</small>
-                  <v-btn class="indigo" outline v-on:click="addAlternative(currentQuestion)" v-if="alternativeLimit(currentQuestion) && lastAlternative(currentQuestion, index)">Adicionar Alternativa</v-btn>
-                  <v-btn class="red" outline v-on:click="removeAlternative(currentQuestion, index)">Remover Alternativa</v-btn>
+                <v-flex class="ma-1 ml-2 pa-1 pl-2" xs3>
+                  <span class="CreateTest-WarningText" v-if="!alternativeLimit(currentQuestion) && lastAlternative(currentQuestion, index)">O numero maximo de alternativas é 5</span>
+                  <v-btn class="indigo" v-on:click="addAlternative(currentQuestion)" v-if="alternativeLimit(currentQuestion) && lastAlternative(currentQuestion, index)" fab small dark><v-icon dark>add</v-icon></v-btn>
+                  <v-btn class="red" v-on:click="removeAlternative(currentQuestion, index)" fab small dark><v-icon dark>remove</v-icon></v-btn>
                 </v-flex>
               </v-layout>
             </div>
-            <v-flex class="ma-1 pa-2" xs6>
-              <v-btn class="green" v-on:click="addQuestion()" v-if="lastQuestion(index)">Adicionar Questão</v-btn>
-              <v-btn class="red" v-on:click="removeQuestion()">Remover Questão</v-btn>
-              <v-btn primary type="submit" v-if="lastQuestion(index)" :loading="loading">Salvar</v-btn>
-            </v-flex>
           </div>
         </form>
       </v-layout>
-    </v-card>
+    </div>
   </div>
 </template>
 <script>
@@ -124,7 +138,7 @@ export default {
       this.questions.forEach(function (question) {
         question.testId = id
         if (!question.isAlternative) {
-          question.keywords = question.keywords.reduce((total, k) => total + ', ' + k)
+          question.keywords = question.keywords.reduce((total, k) => total + ',' + k)
         }
       })
       console.log(this.questions[0].keywords)
@@ -194,10 +208,12 @@ export default {
       return length < 5
     },
     addKeyword () {
-      if (this.keyword !== '') {
+      if (this.keyword !== '' && !(/\s|,|\./g.test(this.keyword)) && /\w{2,}/.test(this.keyword)) {
         this.currentQuestion.keywords.push(this.keyword)
-        this.keyword = ''
+      } else {
+        this.$toastr('error', {position: 'toast-top-right', msg: 'Palavra Chave Invalida!'})
       }
+      this.keyword = ''
     },
     removeChip (word) {
       let index = this.currentQuestion.keywords.findIndex(function (w) {
