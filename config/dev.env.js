de5c3fa149130c5cdc177fd077b7d0c78998850a
Vítor Manfré <vitormd97@gmail.com>
2017-10-19 12:39:17 -0200

    Ajusta rota da API (#83)

--- config/dev.env.js
+++ config/dev.env.js
@@ -3,7 +3,7 @@ var prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  API: '"http://localhost:8080"',
+  // API: '"http://ic-testit.azurewebsites.net"',
   API: '"http://localhost:59927"',
   PDF: '"http://localhost:3000"'
 })
