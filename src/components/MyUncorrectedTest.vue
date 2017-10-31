ce76080f49124a478dec1f4f673bab5ad75a4dff
Medson <medson.oliveira.jr@gmail.com>
2017-10-31 21:32:19 -0200

    Adiciona componente MyUncorrectedTest

---
+++ src/components/MyUncorrectedTest.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="my-uncorrected-test">
+
+  </div>
+</template>
+<script>
+export default {
+  name: 'my-uncorrected-test',
+  props: ['test'],
+  data: () => ({
+
+  }),
+  methods: {
+  }
+}
+</script>
+<style lang="stylus" scoped>
+</style>
