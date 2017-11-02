b1487ab9463e8f942c64bfbd132f8952459162b0
Medson <medson.oliveira.jr@gmail.com>
2017-11-02 03:05:46 -0200

    Adiciona componente grafico media de questoes

---
+++ src/components/AverageQuestionsGraphic.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="average-questions-graphic">
+    <bar-graphic></bar-graphic>
+  </div>
+</template>
+<script>
+import BarGraphic from './BarGraphic'
+
+export default {
+  name: 'average-questions-graphic',
+  data: () => ({
+
+  })
+}
+</script>
+<style lang="stylus" scoped>
+</style>
