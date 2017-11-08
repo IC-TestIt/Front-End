3771f1e77845a0cde36afc7d24fb36765683935d
Medson <medson.oliveira.jr@gmail.com>
2017-11-08 14:28:39 -0200

    Adiciona integração e formato ao grafico

--- src/components/AverageQuestionsGraphic.vue
+++ src/components/AverageQuestionsGraphic.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="average-questions-graphic">
-    <bar-graphic></bar-graphic>
+    <bar-graphic :chart-data="averageQuestions" :height="height" :width="width"></bar-graphic>
   </div>
 </template>
 <script>
@@ -8,9 +8,34 @@ import BarGraphic from './BarGraphic'
 
 export default {
   name: 'average-questions-graphic',
+  components: {
+    BarGraphic
+  },
+  props: ['questions'],
   data: () => ({
-
-  })
+    averageQuestions: {
+      labels: [],
+      datasets: []
+    },
+    height: '190',
+    width: '600'
+  }),
+  beforeMount () {
+    this.getAverageQuestions()
+  },
+  methods: {
+    getAverageQuestions () {
+      this.averageQuestions.labels = this.questions.map(r => r.questionOrder)
+      this.averageQuestions.datasets.push({
+        label: 'Nota',
+        backgroundColor: '#000066',
+        data: []
+      })
+      this.questions.forEach((r) => {
+        this.averageQuestions.datasets[0].data.push(r.classAverage)
+      })
+    }
+  }
 }
 </script>
 <style lang="stylus" scoped>
