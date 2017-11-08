9604e29f41ede0136589634ac70e15f5790045a5
Medson <medson.oliveira.jr@gmail.com>
2017-11-08 15:54:41 -0200

    Adiciona chamada a constants

--- src/components/MyTestTable.vue
+++ src/components/MyTestTable.vue
@@ -15,29 +15,18 @@
   </div>
 </template>
 <script>
+import { statusStudent } from '../utils/constants'
 export default {
   name: 'my-test-table',
   props: ['students', 'room'],
   data: () => ({
+    statusStudent: statusStudent,
     headers: [
       {text: 'ID', value: 'id', align: 'center'},
       {text: 'Nome', value: 'name', align: 'center'},
       {text: 'Status', value: '', align: 'center'}
     ]
-  }),
-  methods: {
-    statusStudent (status) {
-      if (status === 0) {
-        return 'Ausente'
-      } else if (status === 1) {
-        return 'Em Progresso'
-      } else if (status === 2) {
-        return 'Não Corrigido'
-      } else if (status === 3) {
-        return 'Corrigido'
-      }
-    }
-  }
+  })
 }
 </script>
 <style lang="stylus" scoped>
