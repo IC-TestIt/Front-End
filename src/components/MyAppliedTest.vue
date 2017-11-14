617ccb137288b54511b1bdef37c2c0319e84b7d4
Medson <medson.oliveira.jr@gmail.com>
2017-11-14 15:12:06 -0200

    Muda posição do botão

--- src/components/MyAppliedTest.vue
+++ src/components/MyAppliedTest.vue
@@ -7,9 +7,9 @@
           <my-test-table :students="test.students" :room="test.className" height="55vh"></my-test-table>
         </v-flex>
       </v-layout>
-      <v-layout row justify-end>
+      <v-layout row>
         <v-flex xs1>
-          <v-btn class="primary" dark @click="back()">Voltar</v-btn>
+          <v-btn class="primary button-back" dark @click="back()">Voltar</v-btn>
         </v-flex>
       </v-layout>
     </v-container>
@@ -44,6 +44,6 @@ export default {
   max-width 95vw
 
 .button-back
-  margin-left 104px
+  margin-left 0px
 
 </style>
