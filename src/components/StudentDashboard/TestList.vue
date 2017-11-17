14454ece3524b91aee4e42651ccd4dbadf50248c
Medson <medson.oliveira.jr@gmail.com>
2017-11-17 18:49:47 -0200

    Corrige titulo do card

--- src/components/StudentDashboard/TestList.vue
+++ src/components/StudentDashboard/TestList.vue
@@ -1,19 +1,19 @@
 <template>
   <v-card height="74vh" class="test-list">
     <v-flex xs8 class="primary--text display-1 pa-3">
-      Últimas provas
+      Próximas provas
     </v-flex>
     <test-group v-if="todayTests.length" :list="todayTests"></test-group>
-    <test-group v-if="yesterdayTests" :list="yesterdayTests"></test-group>
+    <test-group v-if="tomorrowTests" :list="tomorrowTests"></test-group>
     <test-group v-if="thisWeekTests" :list="thisWeekTests"></test-group>
-    <test-group v-if="olderTests" :list="olderTests"></test-group>
+    <test-group v-if="nextTests" :list="nextTests"></test-group>
   </v-card>
 </template>
 
 <script>
 import testGroup from './TestGroup.vue'
 import {FluentFilterService} from '../../utils/filter'
-import {isToday, isYesterday, isWithinAWeek, isTwoWeeksOrMore, momentDate} from '../../utils/date'
+import {isToday, isTomorrow, isInThisWeek, isInNextWeeks, momentDate} from '../../utils/date'
 
 export default {
   name: 'testList',
@@ -25,17 +25,17 @@ export default {
     return {
       todayTests: [{header: 'Hoje'}],
       thisWeekTests: [{header: 'Esta Semana'}],
-      yesterdayTests: [{header: 'Ontem'}],
-      olderTests: [{header: 'Mais antigas'}]
+      tomorrowTests: [{header: 'Amanhã'}],
+      nextTests: [{header: 'Próximas Semanas'}]
     }
   },
   methods: {
     get () {
       new FluentFilterService(this, this.tests, momentDate, 'endDate')
         .addFilter('todayTests', isToday)
-        .addFilter('yesterdayTests', isYesterday)
-        .addFilter('thisWeekTests', isWithinAWeek)
-        .addFilter('olderTests', isTwoWeeksOrMore)
+        .addFilter('tomorrowTests', isTomorrow)
+        .addFilter('thisWeekTests', isInThisWeek)
+        .addFilter('nextTests', isInNextWeeks)
         .filter()
     }
   },
