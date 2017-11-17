224af622adac2704e29d7e667877f5af5a45cfab
Medson <medson.oliveira.jr@gmail.com>
2017-11-16 23:29:42 -0200

    Adiciona metodo cardColor

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -8,3 +8,14 @@ const studentStates = [
 export const statusStudent = (status) => {
   return studentStates.find(r => r.status === status).description
 }
+
+export const cardColor = (grade) => {
+  let cssClass = 'white--text'
+  if (grade <= 40) {
+    return `red ${cssClass}`
+  } else if (grade <= 60) {
+    return `orange ${cssClass}`
+  } else {
+    return `green darken-1 ${cssClass}`
+  }
+}
