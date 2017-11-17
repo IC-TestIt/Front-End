8b881ab66a02bb484ce21786068de8537d14275f
Medson <medson.oliveira.jr@gmail.com>
2017-11-16 23:30:03 -0200

    Adiciona componente principal do dashboard

---
+++ src/components/StudentDashboard/Main.vue
@@ -0,0 +1,54 @@
+<template lang='html'>
+  <v-container fluid>
+    <v-layout row wrap>
+      <v-flex xs6 class='primary'>
+        <class-list :classes='classes'></class-list>
+      </v-flex>
+      <v-flex xs6>
+        <test-list :tests='tests'></test-list>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+import ClassList from './ClassList'
+import TestList from './TestList'
+import baseService from '../../services/baseService'
+import auth from '../../auth'
+export default {
+  name: 'studentDashboard',
+  components: {
+    ClassList,
+    TestList
+  },
+  data () {
+    return {
+      classes: [],
+      tests: []
+    }
+  },
+  mounted () {
+    this.get()
+  },
+  methods: {
+    get () {
+      let id = auth.studentId()
+      baseService.get(`/student/${id}/classes`).then((r) => {
+        if (r.status === 200) {
+          this.classes = r.data
+          this.classes.forEach((c) => {
+            if (c.correctedStudentTests.length > 0) {
+              c.average = c.correctedStudentTests.map((t) => t.grade)
+                                                 .reduce((total, g) => (total += g)) / c.correctedStudentTests.length
+            }
+          })
+          this.classes.forEach((el) => {
+            this.tests = this.tests.concat(el.correctedStudentTests)
+          })
+        }
+      })
+    }
+  }
+}
+</script>
