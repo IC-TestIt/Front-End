f8ab259056374e650508a2fbb31eaba9259ad5e5
Medson <medson.oliveira.jr@gmail.com>
2017-11-17 18:50:05 -0200

    Adiciona metodos para datas futuras

--- src/utils/date.js
+++ src/utils/date.js
@@ -4,6 +4,8 @@ const REFERENCE = moment()
 const TODAY = REFERENCE.clone().startOf('day')
 const YESTERDAY = REFERENCE.clone().subtract(1, 'days').startOf('day')
 const A_WEEK_OLD = REFERENCE.clone().subtract(7, 'days').startOf('day')
+const TOMORROW = REFERENCE.clone().add(1, 'days').startOf('day')
+const NEXT_WEEK = REFERENCE.clone().add(7, 'days').startOf('day')
 
 export function formatDate (date) {
   return new Date(date).toLocaleDateString('pt-br')
@@ -23,6 +25,18 @@ export function isWithinAWeek (momentDate) {
 
 export function isTwoWeeksOrMore (momentDate) {
   return !isWithinAWeek(momentDate)
+}
+
+export function isInThisWeek (mommentDate) {
+  return momentDate.isBefore(NEXT_WEEK)
+}
+
+export function isTomorrow (momentDate) {
+  return momentDate.isSame(TOMORROW, 'd')
+}
+
+export function isInNextWeeks (momentDate) {
+  return !isInThisWeek(momentDate)
 }
 
 export function momentDate (dateTime) {
