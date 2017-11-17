0cb76d6831d8493fe8c5042d8d0c3ef64ce97ed5
Medson <medson.oliveira.jr@gmail.com>
2017-11-16 23:31:18 -0200

    Adiciona componente ClassList

---
+++ src/components/StudentDashboard/ClassList.vue
@@ -0,0 +1,40 @@
+<template lang="html">
+  <v-container fluid grid-list-lg >
+    <v-layout row wrap class="grades-list">
+      <v-flex xs8 class="white--text display-1">
+        Minhas Turmas
+      </v-flex>
+      <v-flex xs12>
+        <div v-for="c in classes" :key="c.id">
+          <v-flex xs12 class="mb-3">
+            <class-card :room="c"></class-card>
+          </v-flex>
+        </div>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+import ClassCard from './ClassCard'
+export default {
+  name: 'classList',
+  props: ['classes'],
+  components: {
+    ClassCard
+  },
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style lang="stylus">
+
+.grades-list
+  overflow-y auto
+  height 70vh
+
+</style>
