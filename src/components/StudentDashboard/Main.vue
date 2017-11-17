09250e77c7bfdbb1185f86d63156cfe1b9af521e
Medson <medson.oliveira.jr@gmail.com>
2017-11-17 18:48:41 -0200

    Adiciona metodos getTests e getClasses

--- src/components/StudentDashboard/Main.vue
+++ src/components/StudentDashboard/Main.vue
@@ -33,6 +33,18 @@ export default {
   },
   methods: {
     get () {
+      this.getClasses()
+      this.getTests()
+    },
+    getTests () {
+      let id = auth.studentId()
+      baseService.get(`/student/${id}/tests`).then((r) => {
+        if (r.status === 200) {
+          this.tests = r.data
+        }
+      })
+    },
+    getClasses () {
       let id = auth.studentId()
       baseService.get(`/student/${id}/classes`).then((r) => {
         if (r.status === 200) {
@@ -42,9 +54,6 @@ export default {
               c.average = c.correctedStudentTests.map((t) => t.grade)
                                                  .reduce((total, g) => (total += g)) / c.correctedStudentTests.length
             }
-          })
-          this.classes.forEach((el) => {
-            this.tests = this.tests.concat(el.correctedStudentTests)
           })
         }
       })
