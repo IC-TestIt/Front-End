138ed932e7ace1b29ffc1d22aaebaf4b52516deb
Medson <medson.oliveira.jr@gmail.com>
2017-11-18 18:33:41 -0200

    Adiciona componente GradeCard

---
+++ src/components/TestCorrection/GradeCard.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="grade-card">
+    <v-card :class="cardColor(value * 10)">
+      <v-card-title primary-title>
+        <v-flex xs6>
+          <div class="headline bold">{{value}}</div>
+        </v-flex>
+        <v-flex xs6>
+          <div class="bold">{{title}}</div>
+        </v-flex>
+      </v-card-title>
+    </v-card>
+  </div>
+</template>
+<script>
+import {cardColor} from '../../utils/constants'
+export default {
+  name: 'grade-card',
+  props: ['value', 'title'],
+  data () {
+    return {
+      cardColor: cardColor
+    }
+  }
+}
+</script>
+<style lang="stylus" scoped>
+</style>
