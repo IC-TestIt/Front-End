ab7e988d78cb1b5233e475a54de8f88758ff54c9
Medson <medson.oliveira.jr@gmail.com>
2017-11-19 14:55:42 -0200

    Adiciona componente header

---
+++ src/components/MyClass/Header.vue
@@ -0,0 +1,19 @@
+<template lang="html">
+  <v-card class="success white--text text-xs-center">
+    <v-card-title primary-title>
+      <v-flex xs6>
+        <div>Nome da Turma: </div>
+      </v-flex>
+      <v-flex xs6>
+        <div>{{currentClass.name}}</div>
+      </v-flex>
+    </v-card-title>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: 'header',
+  props: ['currentClass']
+}
+</script>
