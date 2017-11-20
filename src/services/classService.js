f574fb278d241c680892bc07b6c626ecbe94dc43
Medson <medson.oliveira.jr@gmail.com>
2017-11-20 01:27:24 -0200

    Adiciona metodos para editar classe

--- src/services/classService.js
+++ src/services/classService.js
@@ -6,5 +6,17 @@ export default {
   },
   getClass () {
     return storage.session.get('class')
+  },
+  saveClassId (id) {
+    storage.session.set('classId', id)
+  },
+  getClassId () {
+    return storage.session.get('classId')
+  },
+  saveEditingClass (c) {
+    storage.session.set('editingClass', c)
+  },
+  getEditingClass () {
+    return storage.session.get('editingClass')
   }
 }
