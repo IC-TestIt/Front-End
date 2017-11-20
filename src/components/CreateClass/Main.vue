c119cb2266b2fb6497db39d8adc0f3ea5c2a03a5
Medson <medson.oliveira.jr@gmail.com>
2017-11-20 01:26:28 -0200

    Adiciona componente principal criar turma

---
+++ src/components/CreateClass/Main.vue
@@ -0,0 +1,22 @@
+<template lang="html">
+  <div class="create-class">
+    <v-container fluid grid-list-lg>
+      <v-layout row wrap>
+        <v-flex xs12>
+          <class-form></class-form>
+        </v-flex>
+      </v-layout>
+    </v-container>
+  </div>
+</template>
+
+<script>
+import ClassForm from './ClassForm'
+
+export default {
+  name: 'create-class',
+  components: {
+    ClassForm
+  }
+}
+</script>
